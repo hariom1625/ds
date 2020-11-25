3ab79db838579cd7b8acd141c2b018c0444d95b5
Hari Om Yadav <66957239+hariom1625@users.noreply.github.com>
2020-11-25 23:52:02 +0530

    QueueLL

---
+++ queue_LL.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+
+	int data;
+	struct Node *next;
+}*front = NULL, *rear = NULL;
+
+void enqueue (int x) {
+
+	struct Node *t;
+	t = (struct Node *)malloc(sizeof(struct Node));
+	if (t == NULL) {
+		cout << "Queue is Full \n";
+	}
+	else {
+		t->data = x;
+		t->next = NULL;
+		if (front == NULL) {
+			front = t;
+			rear = t;
+
+		}
+		else {
+			rear->next = t;
+			rear = t;
+		}
+
+
+	}
+}
+
+int dequeue() {
+
+	int x = -1;
+
+	struct Node *t;
+
+	if (front == NULL) {
+		cout << "Queue is Empty \n";
+	}
+	else {
+		x = front->data;
+		t = front;
+		front = front ->next;
+		free(t);
+	}
+	return x;
+}
+
+void Display() {
+
+	struct Node *p = front;
+
+	while (p) {
+		cout << p->data << " ";
+		p = p->next;
+
+	}
+}
+int main() {
+
+
+
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	enqueue(25);
+	enqueue(16);
+	Display();
+
+}
